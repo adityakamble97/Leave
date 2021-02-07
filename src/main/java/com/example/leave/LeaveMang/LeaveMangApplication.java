f92142f6aba3c337867d976999a7b1726437bd7f
adityakamble97 <adityatheswimmer@gmail.com>
2021-02-07 12:34:53 +0530

    first

---
+++ src/main/java/com/example/leave/LeaveMang/LeaveMangApplication.java
@@ -0,0 +1,15 @@
+package com.example.leave.LeaveMang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveMangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveMangApplication.class, args);
+		System.out.println("running....");
+		
+	}
+
+}
---
+++ src/test/java/com/example/leave/LeaveMang/LeaveMangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leave.LeaveMang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveMangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
